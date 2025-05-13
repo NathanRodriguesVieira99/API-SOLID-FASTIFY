537a73e385d07f5f5a8c227d70f1f4a6df78b270
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-13 17:30:05 -0300

    feat(routes): add Routes

---
+++ src/routes/routes.ts
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { FastifyTypedInstance } from '@/_types/FastifyTypedInstance';
+
+export const Routes = async (server: FastifyTypedInstance) => {};
