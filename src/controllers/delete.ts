80ba988e94f7006c351fb9b029b9cdf33d4c753f
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-13 21:44:44 -0300

    feat(controllers): create deleteUserById controller for user deletion

---
+++ src/controllers/delete.ts
@@ -0,0 +1,24 @@
+import { UsersRepository } from '@/repositories/UsersRepository';
+import { DeleteUseCase } from '@/services/delete';
+import type { FastifyReply, FastifyRequest } from 'fastify';
+
+export async function deleteUserById(
+    request: FastifyRequest,
+    reply: FastifyReply
+) {
+    const { id } = request.params as { id: string };
+
+    try {
+        // chama o repository
+        const usersRepository = new UsersRepository();
+        // injeta o repository no DeleteUseCase
+        const deleteUseCase = new DeleteUseCase(usersRepository);
+
+        await deleteUseCase.execute(id);
+    } catch (error) {
+        // TODO criar uma validação melhor
+        console.error(error, 'error on delete user');
+    }
+
+    return reply.status(204).send();
+}
