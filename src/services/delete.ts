594f92cb4404bd81e0f217b6024c8c46abc4dd81
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-13 21:43:50 -0300

    feat(use-cases): create DeleteUseCase with user deletion by ID

---
+++ src/services/delete.ts
@@ -0,0 +1,15 @@
+/* 
+CASOS DE USO (REGRAS DE NEGÓCIO) DE DELEÇÃO VINDOS DO REPOSITORY 
+*/
+
+import type { UsersRepository } from '@/repositories/UsersRepository';
+
+export class DeleteUseCase {
+    // esse constructor sempre recebe o repository
+    constructor(private usersRepository: UsersRepository) {}
+
+    async execute(id: string): Promise<boolean> {
+        const data = await this.usersRepository.delete(id);
+        return data;
+    }
+}
