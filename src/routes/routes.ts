8116688dcfb8ec1ce160840122aa93cb17f292c4
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-13 22:28:04 -0300

    feat(routes): add swagger documentation

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,41 @@
 /*
-ROTAS SÃO CRIADAS CHAMANDO OS CONTROLLERS E COLOCANDO O NOME DO ENDPOINT
+ROTAS SÃO CRIADAS (E DOCUMENTAS NO SWAGGER) CHAMANDO OS CONTROLLERS E COLOCANDO O NOME DO ENDPOINT
 */
 
 import type { FastifyTypedInstance } from '@/_types/FastifyTypedInstance';
+
+import { z } from 'zod';
+import { registerBodySchema } from '@/validators/registerBodySchema';
+
 import { deleteUserById } from '@/controllers/delete';
 import { register } from '@/controllers/register';
 
 export const Routes = async (server: FastifyTypedInstance) => {
-    server.post('/users', register);
-    server.delete('/users/:id', deleteUserById);
+    server.post(
+        '/users',
+        {
+            schema: {
+                tags: ['users'],
+                description: 'create user',
+                body: registerBodySchema,
+                response: {
+                    201: z.null().describe('user created'),
+                },
+            },
+        },
+        register
+    );
+    server.delete(
+        '/users/:id',
+        {
+            schema: {
+                tags: ['users'],
+                description: 'delete user',
+                response: {
+                    204: z.null().describe('user deleted'),
+                },
+            },
+        },
+        deleteUserById
+    );
 };
