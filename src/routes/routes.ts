48189a362b309485e97b5f86f370c15fe2b9c693
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-15 17:15:52 -0300

    refactor(routes): import the separate groups of routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,83 +3,10 @@ ROTAS SÃO CRIADAS (E DOCUMENTAS NO SWAGGER) CHAMANDO OS CONTROLLERS E COLOCANDO
 */
 import type { FastifyTypedInstance } from '@/_types/FastifyTypedInstance';
 
-import { z } from 'zod';
-import { registerBodySchema } from '@/validators/registerBodySchema';
-
-import { deleteUserById } from '@/controllers/delete';
-import { register } from '@/controllers/register';
-import { listAllUsers } from '@/controllers/listAll';
+import { UserRoutes } from './user-routes/user-routes';
+import { HealthyRoute } from './healthy/healthy';
 
 export const Routes = async (server: FastifyTypedInstance) => {
-    server.get(
-        '/healthy',
-        {
-            schema: {
-                tags: ['healthy '],
-                description: ' checking project healthy ',
-                response: {
-                    200: z
-                        .object({
-                            status: z.string(),
-                            message: z.string(),
-                        })
-                        .describe('server is running'),
-                },
-            },
-        },
-        async () => {
-            return { status: 'ok', message: 'Server is running' };
-        }
-    );
-    server.post(
-        '/users',
-        {
-            schema: {
-                tags: ['users'],
-                description: 'create user',
-                body: registerBodySchema,
-                response: {
-                    201: z.null().describe('user created'),
-                },
-            },
-        },
-        register
-    );
-    server.delete(
-        '/users/:id',
-        {
-            schema: {
-                tags: ['users'],
-                description: 'delete user',
-                response: {
-                    204: z.null().describe('user deleted'),
-                },
-            },
-        },
-        deleteUserById
-    );
-    server.get(
-        '/users',
-        {
-            schema: {
-                tags: ['users'],
-                description: 'list all users',
-                response: {
-                    200: z.object({
-                        users: z.array(
-                            z.object({
-                                id: z.string().uuid(),
-                                name: z.string(),
-                                email: z.string().email(),
-                                password_hash: z.string(),
-                                created_at: z.date(),
-                                updated_at: z.date(),
-                            })
-                        ),
-                    }),
-                },
-            },
-        },
-        listAllUsers
-    );
+    server.register(HealthyRoute);
+    server.register(UserRoutes);
 };
