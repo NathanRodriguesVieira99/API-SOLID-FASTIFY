f707d6ec3ec57ade4593f300f2d6d00a1c8d9c80
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-15 17:22:07 -0300

    feat(errors): upadate error handling

--- src/controllers/listAll.ts
+++ src/controllers/listAll.ts
@@ -1,5 +1,7 @@
 import type { FastifyReply, FastifyRequest } from 'fastify';
 
+import { UsersNoExistsError } from '@/services/errors/users-no-exist-error';
+
 import { UsersRepository } from '@/repositories/UsersRepository';
 
 import { ListAllUsersUseCase } from '@/services/listAll';
@@ -17,9 +19,13 @@ export async function listAllUsers(
         // busca os users para listar
         const { users } = await listAllUsersUseCase.execute();
 
+        // se não houver erros lista com sucesso
         return reply.status(200).send({ users });
     } catch (error) {
-        // TODO criar validação melhor
+        // verifica todos os possíveis erros
+        if (error instanceof UsersNoExistsError) {
+            return reply.status(404).send({ message: error.message });
+        }
         console.error(error);
         return reply.status(500).send({
             error: 'internal server error',
