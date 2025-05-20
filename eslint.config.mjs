2fd04d0eec70c24645404eac067be9a3fc9d7046
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-19 22:20:18 -0300

    feat(eslint): add imports plugin configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,58 @@
 import js from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
 import json from '@eslint/json';
 import { defineConfig } from 'eslint/config';
+import importPlugin from 'eslint-plugin-import'; 
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
     {
         files: ['**/*.{js,mjs,cjs,ts}'],
-        plugins: { js },
+        plugins: {
+            js,
+            import: importPlugin,
+        },
         extends: ['js/recommended'],
+        rules: {
+            'import/order': [
+                'error',
+                {
+                    groups: [
+                        'builtin',
+                        'external',
+                        'internal',
+                        'parent',
+                        'sibling',
+                        'index',
+                    ],
+                    pathGroups: [
+                        {
+                            pattern: '@/**',
+                            group: 'internal',
+                        },
+                    ],
+                    alphabetize: { order: 'asc', caseInsensitive: true },
+                    'newlines-between': 'always',
+                },
+            ],
+            'import/no-unresolved': 'error',
+            'import/no-duplicates': 'error',
+        },
+
+        settings: {
+            'import/resolver': {
+                typescript: true,
+            },
+        },
     },
     {
         files: ['**/*.{js,mjs,cjs,ts}'],
-        languageOptions: { globals: globals.node },
+        languageOptions: {
+            globals: globals.node,
+            parserOptions: {
+                sourceType: 'module',
+            },
+        },
     },
     tseslint.configs.recommended,
     {
