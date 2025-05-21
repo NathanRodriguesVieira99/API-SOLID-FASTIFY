4dc5da04acdf42de5e35aaba5a57133fe7653f60
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-21 11:59:31 -0300

    test(register): create test to verify user password hash is working correctly

--- src/core/useCases/register.test.ts
+++ src/core/useCases/register.test.ts
@@ -1,3 +1,32 @@
+import { compare } from 'bcryptjs';
+
+import { InMemoryUsersRepository } from '@/infrastructure/db/repositories/in-memory/InMemoryUsersRepository';
+
+import { RegisterUseCase } from './register';
+
 describe('POST /users ', () => {
-    it('should ', () => {});
+    const userMock = {
+        name: 'Jhon Doe',
+        email: 'JhonDoe@example.com',
+        password: '12345678',
+    };
+
+    it('should hash user password upon registration ', async () => {
+        // chamo o repositório em memória e injeto no UseCase
+        const usersRepository = new InMemoryUsersRepository();
+        const registerUseCase = new RegisterUseCase(usersRepository);
+
+        // executo o método de criar User com um user mockado
+        const { user } = await registerUseCase.execute(userMock);
+
+        // comparo se a senha do user corresponde a senha com hash criada pelo bcryptjs
+        const isPasswordCorrectlyHashed = await compare(
+            userMock.password,
+            user.password_hash
+        );
+
+        expect(isPasswordCorrectlyHashed).toBe(true);
+    });
+
+    it('should not be able to register with the same email twice', () => {});
 });
